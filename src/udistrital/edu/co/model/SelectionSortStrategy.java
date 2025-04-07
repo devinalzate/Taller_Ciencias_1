adc7a464aa265f1ecd32c943ac693fcc61ef7d38
Alejandro-049 <eaorjuelao@udistrital.edu.co>
2025-04-06 21:55:22 -0500

    SelectionSort agregado

---
+++ src/udistrital/edu/co/model/SelectionSortStrategy.java
@@ -0,0 +1,59 @@
+package udistrital.edu.co.model;
+
+public class SelectionSortStrategy implements SortStrategy {
+    private int comparaciones;
+    private int movimientos;
+    private long tiempoEjecucion;
+
+    @Override
+    public Politico[] ordenarArreglo(Politico[] politicos) {
+        movimientos = 0;
+        comparaciones = 0;
+        long tiempoInicial = System.nanoTime();
+
+        Politico[] politicos_copia = politicos.clone();
+        int n = politicos_copia.length;
+
+        for(int i = 0; i < n-1; i++) {
+            int minimo = i;
+            for(int j = i+1; j < n; j++) {
+                comparaciones++;
+                if(politicos_copia[j].getValor_a_robar() < politicos_copia[minimo].getValor_a_robar()) {
+                    minimo = j;
+                }
+            }
+
+            if(minimo != i) {
+                Politico auxiliar = politicos_copia[minimo];
+                politicos_copia[minimo] = politicos_copia[i];
+                politicos_copia[i] = auxiliar;
+                movimientos++;
+            }
+        }
+
+        long tiempoFinal = System.nanoTime();
+        tiempoEjecucion = tiempoFinal - tiempoInicial;
+
+        return politicos_copia;
+    }
+
+    @Override
+    public void ordenarMatriz(Politico[][] matriz) {
+
+    }
+
+    @Override
+    public int getComparaciones() {
+        return this.comparaciones;
+    }
+
+    @Override
+    public int getMovimientos() {
+        return this.movimientos;
+    }
+
+    @Override
+    public long getTiempoEjecucion() {
+        return this.tiempoEjecucion;
+    }
+}
